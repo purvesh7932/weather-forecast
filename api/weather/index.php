dc886842f7ba6fe413b3bb32c7458f24d14d9783
ankurbiz <ankurr@biztechnosys.com>
2020-04-06 00:53:34 +0530

    api weather php

---
+++ api/weather/index.php
@@ -0,0 +1,32 @@
+<?php
+$servername = "localhost";
+$username   = "root";
+$password   = "";
+$dbname     = "weather";
+
+header("Access-Control-Allow-Origin: *");
+header('Content-type: application/json');
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
+
+  //echo "Connected successfully";
+$sql = "SELECT * FROM city";
+$result = mysqli_query($conn,$sql); 
+$myArray = array();
+if ($result->num_rows > 0) {
+// output data of each row
+    while($row = $result->fetch_assoc()) {
+        $myArray[] = $row;
+    }
+    print json_encode($myArray);
+} 
+else 
+{
+   echo "0 results";
+}
+?>
